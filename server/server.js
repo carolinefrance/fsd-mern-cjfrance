5a0c0e927732d6a9af8b6cdcfbf43aaa5b84379c
Caroline France <107187127+carolinefrance@users.noreply.github.com>
2023-05-15 16:38:47 -0400

    Updated server.js to be compatible with Heroku
    Added const path and if(process.env...) block to be compatible with Heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const connectDb = require('./src/database');
 const faker = require('faker');
+const path = require('path');
 
 const User = require('./src/models/user.model');
 
@@ -35,6 +36,16 @@ app.get('/users-delete', async (req, res) => {
 app.get('/', (req, res) => {
   res.send('Hello from Node.js app \n');
 });
+
+// Serve static assets (build folder) if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // start server
 const PORT = process.env.PORT || 8080;
@@ -42,49 +53,3 @@ app.listen(PORT, function () {
   console.log(`Server is running on port ${PORT}! - http://localhost:${PORT}`);
   connectDb().then(() => console.log('MongoDb connected'));
 });
-
-/* OLD CODE
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const connectDb = require('./src/database');
-const faker = require('faker');
-
-const User = require('./src/models/user.model');
-
-// configure express to use cors()
-app.use(cors());
-
-app.get('/users', async (req, res) => {
-  const users = await User.find();
-
-  res.json(users);
-});
-
-app.get('/user-create', async (req, res) => {
-  const user = new User({
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-  });
-
-  await user.save().then(() => console.log('User created'));
-
-  res.send('User created \n');
-});
-
-app.get('/users-delete', async (req, res) => {
-  await User.deleteMany({}).then(() => console.log('Users deleted'));
-
-  res.send('Users deleted \n');
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js app \n');
-});
-
-// start server
-app.listen(8080, function () {
-  console.log('Server is running on port 8080! - http://localhost:8080');
-  connectDb().then(() => console.log('MongoDb connected'));
-});
-*/
